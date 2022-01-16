1808bfb77b7806c91bffe38ac6c86c12b75c4600
Nookta <96252282+Nookta@users.noreply.github.com>
2022-01-16 09:09:18 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,11 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+         Yaşam standardı ve kalitesi bakımından Türkiye’nin en yüksek standartta yaşam vadeden şehirlerinin başında gelen İzmir,<br>
+    hemen her alanda bölgede ikamet edenlere özel fırsatlar sunar. <a href="https://bilgebakimevi.com/" title="izmir huzurevi">izmir huzurevi</a> arayışında olanlar için de bu kapsamda değerlendirilebilecek <br>
+    alternatifler yoğun bir biçimde bulunur. Tercih yelpazesinin geniş olmasına karşın yaşlı ve bakıma muhtaç sevdiklerinizin en yüksek standartta <br>
+    hizmet alabilmesi ve yaşamlarını yüksek kaliteli standartlarda sürdürebilmesi adına <br>
+    huzurevi seçerken bazı püf noktalara yeteri kadar özen göstermesi gerekmektedir. 
         </p>
       </main>
 
